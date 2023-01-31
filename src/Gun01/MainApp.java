605a8962d8a2de1d36eb33398655ce5c47b899b2
TechnoStudy <ismettechno@gmail.com>
2023-01-31 18:50:56 +0000

    ilk selenium sayfası açıldı

--- src/Gun01/MainApp.java
+++ src/Gun01/MainApp.java
@@ -1,7 +1,17 @@
 package Gun01;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class MainApp {
     public static void main(String[] args) {
+//        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new EdgeDriver();
+        //WebDriver driver = new SafariDriver();
 
+        driver.get("https://www.google.com");
     }
 }
